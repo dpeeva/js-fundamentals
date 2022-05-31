9e594a9ce89b7c3dff9beb37042c4f8ed0a70784
Desislava <desi.peeva@gmail.com>
2022-05-31 20:42:11 +0300

    add solution to Chars to String

---
+++ 02_DataTypesVariables/hw/charsToString.js
@@ -0,0 +1,21 @@
+function solve(a, b, c) {
+    console.log(`${a}${b}${c}`)
+}
+
+solve(
+    'a',
+    'b',
+    'c'
+) // abc
+
+solve(
+    '%',
+    '2',
+    'o'
+) // %2o
+
+solve(
+    '1',
+    '5',
+    'p'
+) // 15p
