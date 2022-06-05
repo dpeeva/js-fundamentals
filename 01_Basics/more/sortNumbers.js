8bda1fefc8a8632a369bc5b351514523394436da
Desislava <desi.peeva@gmail.com>
2022-06-05 15:46:08 +0300

    add solution to Sort Numbers

---
+++ 01_Basics/more/sortNumbers.js
@@ -0,0 +1,28 @@
+function solve(n1, n2, n3) {
+    const arr = [n1, n2, n3]
+    const sortedArr = []
+    let max = 0
+    let indexOfMax = 0
+
+    for (let i = 0; i < arguments.length; i++) {
+        max = Math.max(...arr)
+        console.log(max)
+        indexOfMax = arr.indexOf(max)
+        arr.splice(indexOfMax, 1)
+    }
+}
+
+solve(2, 1, 3)
+// 3
+// 2
+// 1
+
+solve(-2, 1, 3)
+// 3
+// 1
+// -2
+
+solve(0, 0, 2)
+// 2
+// 0
+// 0
