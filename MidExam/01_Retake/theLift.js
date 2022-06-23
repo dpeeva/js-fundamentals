0c820f4272cd13287ca0fecabbd58c3452144b31
Desislava <desi.peeva@gmail.com>
2022-06-23 20:03:14 +0300

    add 90 of 100 solution to The Lift

---
+++ MidExam/01_Retake/theLift.js
@@ -0,0 +1,50 @@
+// 90 of 100
+function solve(input) {
+    const people = Number(input[0])
+    let waiting = Number(input[0])
+    const lift = input[1].split(" ").map(Number)
+
+    let index = 0
+    let isFull = false
+
+    while (index < people) {
+        for (let wagon = 0; wagon < lift.length; wagon++) {
+            if (lift[wagon] < 4) {
+                lift[wagon] += 1
+                waiting--
+                break
+            }
+            if (wagon === lift.length - 1 && lift[wagon] === 4) {
+                isFull = true
+            }
+        }
+
+        if (isFull) {
+            break
+        }
+        index++
+    }
+
+    if (isFull) {
+        if (waiting > 0) {
+            console.log(`There isn't enough space! ${waiting} people in a queue!`)
+        }
+    } else {
+        console.log(`The lift has empty spots!`)
+    }
+    console.log(lift.join(" "))
+}
+
+solve([
+    "15",
+    "0 0 0 0 0"
+])
+// The lift has empty spaces!
+// 4 4 4 3 0
+
+solve([
+    "20",
+    "0 2 0"
+])
+// There isn't enough space! 10 people in a queue!
+// 4 4 4
