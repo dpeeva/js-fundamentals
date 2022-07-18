b855a3b0f69a3794c9fe0972d0a6f041317b7469
Desislava <desi.peeva@gmail.com>
2022-07-18 22:21:00 +0300

    add solution to Print Characters

---
+++ 08_TextProcessing/lab/printCharacters.js
@@ -0,0 +1,26 @@
+function solve(input) {
+    // for (let i = 0; i < input.length; i++) {
+    //     console.log(input[i])
+    // }
+
+    for (const char of input) {
+        console.log(char)
+    }
+}
+
+solve('AWord')
+// A
+// W
+// o
+// r
+// d
+
+solve('Sentence')
+// S
+// e
+// n
+// t
+// e
+// n
+// c
+// e
