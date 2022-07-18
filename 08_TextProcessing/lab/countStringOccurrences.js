791aa8a5a5c5d03dfda9c34e3be0bf93a0f815ff
Desislava <desi.peeva@gmail.com>
2022-07-18 23:17:43 +0300

    add solution for Count String occurrences

---
+++ 08_TextProcessing/lab/countStringOccurrences.js
@@ -0,0 +1,15 @@
+function solve(text, word) {
+    console.log(
+        text.split(" ").filter(w => w === word).length
+    )
+}
+
+solve(
+    'This is a word and it also is a sentence',
+    'is'
+) // 2
+
+solve(
+    'softuni is great place for learning new programming languages',
+    'softuni'
+) // 1
